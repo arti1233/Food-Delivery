0877964c8ee4192ee083ebd0ab2c51eac6628c41
Artsiom <107428500+arti1233@users.noreply.github.com>
2022-10-16 15:02:27 +0300

    Add files via upload

---
+++ Delivery/Delivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Delivery
+//
+//  Created by Artsiom Korenko on 16.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
